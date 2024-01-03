592f3d94fd0385a014086eda83ac133c4cb06741
Dmitriy199708 <119492971+Dmitriy199708@users.noreply.github.com>
2024-01-03 21:16:30 +0300

    add project

---
+++ src/test/java/ru/netology/selenium/web/RegistrationTestNegative.java
@@ -0,0 +1,261 @@
+package ru.netology.selenium.web;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationTestNegative {
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @BeforeEach
+    void setUp() {
+        openSite();
+    }
+
+    void openSite() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void registeringUserWithAnInvalidCity() {
+        $x("//span[@data-test-id='city']//input").setValue("Крокус");
+        String planningData = generateDate(3, "dd.MM.yyyy");
+        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $x("//span[@data-test-id='date']//input").setValue(planningData);
+        $x("//span[@data-test-id='name']//input").setValue("Дмитрий Лютиков");
+        $x("//span[@data-test-id='phone']//input").setValue("+79200077999");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//span[@class='button__text']").click();
+        $x("//span[@data-test-id='city'] //span[@class='input__sub']")
+                .shouldBe(visible)
+                .shouldBe(Condition.exactText("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void registeringUserWithAnInvalidCityLatinTerms() {
+        $x("//span[@data-test-id='city']//input").setValue("Ggdqdc");
+        String planningData = generateDate(3, "dd.MM.yyyy");
+        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $x("//span[@data-test-id='date']//input").setValue(planningData);
+        $x("//span[@data-test-id='name']//input").setValue("Дмитрий Лютиков");
+        $x("//span[@data-test-id='phone']//input").setValue("+79200077999");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//span[@class='button__text']").click();
+        $x("//span[@data-test-id='city'] //span[@class='input__sub']")
+                .shouldBe(visible)
+                .shouldBe(Condition.exactText("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void userRegistrationWithoutCity() {
+        String planningData = generateDate(3, "dd.MM.yyyy");
+        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $x("//span[@data-test-id='date']//input").setValue(planningData);
+        $x("//span[@data-test-id='name']//input").setValue("Дмитрий Лютиков");
+        $x("//span[@data-test-id='phone']//input").setValue("+79200077999");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//span[@class='button__text']").click();
+        $x("//span[@data-test-id='city'] //span[@class='input__sub']")
+                .shouldBe(visible)
+                .shouldBe(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void registeringUserWithAnInvalidDate() {
+        $x("//span[@data-test-id='city']//input").setValue("Москва");
+        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $x("//span[@data-test-id='date'] //input[@class='input__control']").sendKeys("20.01.2023");
+        $x("//span[@data-test-id='name']//input").setValue("Иванов-Иванович Иван");
+        $x("//span[@data-test-id='phone']//input").setValue("+79200077999");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//span[@class='button__text']").click();
+        $x("//span[@data-test-id='date'] //span[@class='input__sub']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Заказ на выбранную дату невозможен"));
+    }
+
+    @Test
+    void userRegistrationWithDateInmmddyyFormat() {
+        $x("//span[@data-test-id='city']//input").setValue("Москва");
+        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $x("//span[@data-test-id='date'] //input[@class='input__control']").sendKeys("20.01.24");
+        $x("//span[@data-test-id='name']//input").setValue("Иванов-Иванович Иван");
+        $x("//span[@data-test-id='phone']//input").setValue("+79200077999");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//span[@class='button__text']").click();
+        $x("//span[@data-test-id='date'] //span[@class='input__sub']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Неверно введена дата"));
+    }
+
+    @Test
+    void userRegistrationWithDateOfFebruary30() {
+        $x("//span[@data-test-id='city']//input").setValue("Москва");
+        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $x("//span[@data-test-id='date'] //input[@class='input__control']").sendKeys("30.02.2024");
+        $x("//span[@data-test-id='name']//input").setValue("Иванов-Иванович Иван");
+        $x("//span[@data-test-id='phone']//input").setValue("+79200077999");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//span[@class='button__text']").click();
+        $x("//span[@data-test-id='date'] //span[@class='input__sub']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Неверно введена дата"));
+    }
+
+    @Test
+    void userRegistrationWithDateBlankDate() {
+        $x("//span[@data-test-id='city']//input").setValue("Москва");
+        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $x("//span[@data-test-id='name']//input").setValue("Иванов-Иванович Иван");
+        $x("//span[@data-test-id='phone']//input").setValue("+79200077999");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//span[@class='button__text']").click();
+        $x("//span[@data-test-id='date'] //span[@class='input__sub']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Неверно введена дата"));
+    }
+
+    @Test
+    void registeringUserWithAnInvalidName() {
+        $x("//span[@data-test-id='city']//input").setValue("Москва");
+        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $x("//span[@data-test-id='date'] //input[@class='input__control']").sendKeys("20.01.2024");
+        $x("//span[@data-test-id='name']//input").setValue("Дмитрий1");
+        $x("//span[@data-test-id='phone']//input").setValue("+79200077999");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//span[@class='button__text']").click();
+        $x("//span[@data-test-id='name'] //span[@class='input__sub']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void registeringUserWitNameInLatin() {
+        $x("//span[@data-test-id='city']//input").setValue("Москва");
+        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $x("//span[@data-test-id='date'] //input[@class='input__control']").sendKeys("20.01.2024");
+        $x("//span[@data-test-id='name']//input").setValue("Gdergv");
+        $x("//span[@data-test-id='phone']//input").setValue("+79200077999");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//span[@class='button__text']").click();
+        $x("//span[@data-test-id='name'] //span[@class='input__sub']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void registeringUserWithAnEmptyName() {
+        $x("//span[@data-test-id='city']//input").setValue("Москва");
+        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $x("//span[@data-test-id='date'] //input[@class='input__control']").sendKeys("20.01.2024");
+        $x("//span[@data-test-id='phone']//input").setValue("+79200077999");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//span[@class='button__text']").click();
+        $x("//span[@data-test-id='name'] //span[@class='input__sub']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Поле обязательно для заполнения "));
+    }
+
+    @Test
+    void registeringUserWithPhoneNumberStartingWith8() {
+        $x("//span[@data-test-id='city']//input").setValue("Москва");
+        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $x("//span[@data-test-id='date'] //input[@class='input__control']").sendKeys("20.01.2024");
+        $x("//span[@data-test-id='name']//input").setValue("Иванов-аглы Дмитрий");
+        $x("//span[@data-test-id='phone']//input").setValue("879200077999");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//span[@class='button__text']").click();
+        $x("//span[@data-test-id='phone'] //span[@class='input__sub']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void userRegistrationForNonExistentNumber() {
+        $x("//span[@data-test-id='city']//input").setValue("Москва");
+        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $x("//td[@class='calendar__day' and @data-day='1705438800000'] ").click();
+        String data = $x("//span[@data-test-id='date'] //input[@type='tel']").val();
+        $x("//span[@data-test-id='name']//input").setValue("Иванов-аглы Дмитрий");
+        $x("//span[@data-test-id='phone']//input").setValue("+70000000000");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//span[@class='button__text']").click();
+        $x("//div[@class='notification__content']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Встреча успешно забронирована на " + data));
+    }
+
+    @Test
+    void registeringUserWithAnInvalidPhone() {
+        $x("//span[@data-test-id='city']//input").setValue("Москва");
+        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $x("//span[@data-test-id='date'] //input[@class='input__control']").sendKeys("20.01.2024");
+        $x("//span[@data-test-id='name']//input").setValue("Дмитрий");
+        $x("//span[@data-test-id='phone']//input").setValue("+792000779991");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//span[@class='button__text']").click();
+        $x("//span[@data-test-id='phone'] //span[@class='input__sub']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+
+    @Test
+    void userRegistrationWithoutAgreeingToTheTermsAndConditions() {
+        $x("//span[@data-test-id='city']//input").setValue("Москва");
+        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $x("//span[@data-test-id='date'] //input[@class='input__control']").sendKeys("20.01.2024");
+        $x("//span[@data-test-id='name']//input").setValue("Дмитрий");
+        $x("//span[@data-test-id='phone']//input").setValue("+79200077999");
+        $x("//span[@class='button__text']").click();
+        $x("//label[@data-test-id='agreement'] //span[@class='checkbox__text']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+    }
+
+    @Test
+    void shouldCardDeliverySuccessCityList() {
+        // Выбор даты на неделю вперёд, начиная от текущей даты, через инструмент календаря:
+        LocalDate dateNow = LocalDate.now();
+        LocalDate plusWeek = dateNow.plusWeeks(1);
+        LocalDate availableDay = dateNow.plusDays(3);
+
+        boolean isSameYear = dateNow.getYear() == plusWeek.getYear();
+        boolean isMonthDiff = dateNow.getMonthValue() < plusWeek.getMonthValue();
+        boolean availableStartSameMonth = availableDay.getMonthValue() == dateNow.getMonthValue();
+
+        boolean needToChangeMonth = availableStartSameMonth && (!isSameYear || isMonthDiff);
+
+        $("button[type='button']").click();
+
+        if (needToChangeMonth) {
+            $(".calendar__arrow_direction_right[data-step='1']").click();
+        }
+
+        $$("td.calendar__day").find(text(String.valueOf(plusWeek.getDayOfMonth()))).click();
+        // Остальные поля
+        $("input[type='text']").setValue("Мо");
+        $(withText("Москва")).click();
+        $("input[name='name']").setValue("Василий Иванович");
+        $("input[name='phone']").setValue("+79990873456");
+        $x("//label[@data-test-id='agreement'] //span[@class='checkbox__text']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+
+    }
+
+}
+
---
+++ src/test/java/ru/netology/selenium/web/RegistrationTestPositive.java
@@ -0,0 +1,102 @@
+package ru.netology.selenium.web;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.*;
+import static java.lang.String.valueOf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class RegistrationTestPositive {
+
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @BeforeEach
+    void setUp() {
+        openSite();
+    }
+
+    void openSite() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldRegisterByAccountNumberDOMModification() {
+        $x("//span[@data-test-id='city']//input").setValue("Нижний Новгород");
+        String planningData = generateDate(3, "dd.MM.yyyy");
+        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $x("//span[@data-test-id='date']//input").setValue(planningData);
+        $x("//span[@data-test-id='name']//input").setValue("Дмитрий Лютиков");
+        $x("//span[@data-test-id='phone']//input").setValue("+79200077999");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//span[@class='button__text']").click();
+        $x("//div[@class='notification__content']")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Встреча успешно забронирована на " + planningData));
+    }
+
+    @Test
+    void successfulUserRegistrationOnTheSelectedDate() {
+        $x("//span[@data-test-id='city']//input").setValue("Москва");
+        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        String date = ("22.01.2024");
+        $x("//span[@data-test-id='date']//input").setValue(date);
+        $x("//span[@data-test-id='name']//input").setValue("Иванов-Иванович Иван");
+        $x("//span[@data-test-id='phone']//input").setValue("+79200077999");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//span[@class='button__text']").click();
+        $x("//div[@class='notification__content']")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Встреча успешно забронирована на " + date));
+    }
+
+    @Test
+    void successfulUserRegistrationOnTheSelectedDateFromTheCalendar() {
+        $x("//span[@data-test-id='city']//input").setValue("Москва");
+        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $x("//span[@data-test-id='date'] //span[@class='icon-button__content']").click();
+        $x("//td[@class='calendar__day' and @data-day='1705438800000'] ").click();
+        String data = $x("//span[@data-test-id='date'] //input[@type='tel']").val();
+        $x("//span[@data-test-id='name']//input").setValue("Иванов-Иванович Иван");
+        $x("//span[@data-test-id='phone']//input").setValue("+79200077999");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//span[@class='button__text']").click();
+        $x("//div[@class='notification__content']")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Встреча успешно забронирована на " + data));
+    }
+
+    @Test
+    void successfulUserRegistrationForTheSelectedCityFromTheDropDownList() {
+        $x("//span[@data-test-id='city']//input").setValue("Ке");
+        $x("//span[@class='menu-item__control' and text()='Кемерово']").click();
+        $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        String date = ("22.01.2024");
+        $x("//span[@data-test-id='date']//input").setValue(date);
+        $x("//span[@data-test-id='name']//input").setValue("Иванов-Иванович Иван");
+        $x("//span[@data-test-id='phone']//input").setValue("+79200077999");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//span[@class='button__text']").click();
+        $x("//div[@class='notification__content']")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Встреча успешно забронирована на " + date));
+    }
+}
+
